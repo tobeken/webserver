153e46118ce25f693fff8bea646b20b063ce4698
tobeken <tobik0124@gmail.com>
2022-04-03 08:57:39 +0900

    heroku deploy

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.get('/about', function (req, res) {
     res.send('About page')
   })
 
-app.listen(3000,()=>{
+const PORT = process.env.PORT || 3000
+app.listen(PORT,()=>{
     console.log("I'm running!")
 })
